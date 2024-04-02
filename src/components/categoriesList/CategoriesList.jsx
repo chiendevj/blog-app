0831d2280df4943a64b8e9c8e8febcfc0870e3ad
ttchien <chientt.00.student@fit.tdc.edu.vn>
2024-04-02 18:12:41 +0700

    add component

---
+++ src/components/categoriesList/CategoriesList.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import styles from './categoriesList.module.css'
+const CategoriesList = () => {
+    return (
+        <div className={styles.container}> Categories</div>
+    );
+}
+
+export default CategoriesList
