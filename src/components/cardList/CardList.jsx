0f54c0bfb8427212427d78e82c4ec516fa8e5cb6
ttchien <chientt.00.student@fit.tdc.edu.vn>
2024-04-03 22:43:58 +0700

    done UI

--- src/components/cardList/CardList.jsx
+++ src/components/cardList/CardList.jsx
@@ -1,37 +1,83 @@
 import React from "react";
 import styles from './cardList.module.css'
 import Image from "next/image";
+import Link from "next/link";
 import Pagination from "../pagination/Pagination";
 const CardList = () => {
     return (
         <>
             <div className={`${styles.container} ${styles.cardlist}`}>
-                <h2 className={styles.titlePosts}>Popular Posts</h2>
+                <h2 className={styles.titlePosts}>Recent Posts</h2>
                 <div className={styles.posts}>
                     <div className={styles.post}>
                         <div className={styles.imagePost}>
                             <Image className={styles.image} src={'/culture.png'} alt={'post'} fill />
                         </div>
                         <div className={styles.contentPost}>
+                            <p className={styles.typeCategory}>2024-04-03 &#8226; <span>CODING</span></p>
                             <h3 className={styles.titlePost}>Post 1</h3>
                             <p className={styles.description}>
                                 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias illum architecto rerum deserunt aperiam impedit esse. Odit similique libero vitae numquam, molestiae id assumenda deserunt ad necessitatibus. Inventore, tenetur incidunt.
                             </p>
+                            <Link className={styles.readMore} href={'/'}>Read more</Link>
+
                         </div>
                     </div>
-                    
                     <div className={styles.post}>
                         <div className={styles.imagePost}>
                             <Image className={styles.image} src={'/culture.png'} alt={'post'} fill />
                         </div>
                         <div className={styles.contentPost}>
+                            <p className={styles.typeCategory}>2024-04-03 &#8226; <span>CODING</span></p>
                             <h3 className={styles.titlePost}>Post 1</h3>
                             <p className={styles.description}>
                                 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias illum architecto rerum deserunt aperiam impedit esse. Odit similique libero vitae numquam, molestiae id assumenda deserunt ad necessitatibus. Inventore, tenetur incidunt.
                             </p>
+                            <Link className={styles.readMore} href={'/'}>Read more</Link>
+
                         </div>
                     </div>
-                    
+                    <div className={styles.post}>
+                        <div className={styles.imagePost}>
+                            <Image className={styles.image} src={'/culture.png'} alt={'post'} fill />
+                        </div>
+                        <div className={styles.contentPost}>
+                            <p className={styles.typeCategory}>2024-04-03 &#8226; <span>CODING</span></p>
+                            <h3 className={styles.titlePost}>Post 1</h3>
+                            <p className={styles.description}>
+                                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias illum architecto rerum deserunt aperiam impedit esse. Odit similique libero vitae numquam, molestiae id assumenda deserunt ad necessitatibus. Inventore, tenetur incidunt.
+                            </p>
+                            <Link className={styles.readMore} href={'/'}>Read more</Link>
+
+                        </div>
+                    </div>
+                    <div className={styles.post}>
+                        <div className={styles.imagePost}>
+                            <Image className={styles.image} src={'/culture.png'} alt={'post'} fill />
+                        </div>
+                        <div className={styles.contentPost}>
+                            <p className={styles.typeCategory}>2024-04-03 &#8226; <span>CODING</span></p>
+                            <h3 className={styles.titlePost}>Post 1</h3>
+                            <p className={styles.description}>
+                                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias illum architecto rerum deserunt aperiam impedit esse. Odit similique libero vitae numquam, molestiae id assumenda deserunt ad necessitatibus. Inventore, tenetur incidunt.
+                            </p>
+                            <Link className={styles.readMore} href={'/'}>Read more</Link>
+
+                        </div>
+                    </div>
+                    <div className={styles.post}>
+                        <div className={styles.imagePost}>
+                            <Image className={styles.image} src={'/culture.png'} alt={'post'} fill />
+                        </div>
+                        <div className={styles.contentPost}>
+                            <p className={styles.typeCategory}>2024-04-03 &#8226; <span>CODING</span></p>
+                            <h3 className={styles.titlePost}>Post 1</h3>
+                            <p className={styles.description}>
+                                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias illum architecto rerum deserunt aperiam impedit esse. Odit similique libero vitae numquam, molestiae id assumenda deserunt ad necessitatibus. Inventore, tenetur incidunt.
+                            </p>
+                            <Link className={styles.readMore} href={'/'}>Read more</Link>
+                        </div>
+                    </div>
                 </div>
                 <Pagination />
             </div>
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,50 @@
 import React from "react";
 import styles from './footer.module.css'
+import Image from "next/image";
+import Link from "next/link";
 const Footer = () => {
     return (
-        <footer className={styles.container}>Footer</footer>
+        <footer className={styles.container}>
+            <div className={styles.contact}>
+                <div className={styles.info}>
+                    <Image className={styles.image} src={'/style.png'} width={50} height={50}/>
+                    <div className={styles.username}>Chiendevj</div>
+                </div>
+                <p className={styles.description}>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum distinctio voluptatem praesentium, fuga eius quod adipisci sed dolores, asperiores, vel temporibus aperiam. Ex minus aliquam expedita eius officiis vero voluptatum?
+                </p>
+                <div className={styles.social}>
+                    <Image src="/facebook.png" alt="facebook" width={24} height={24} />
+                    <Image src="/instagram.png" alt="instagram" width={24} height={24} />
+                    <Image src="/youtube.png" alt="youtube" width={24} height={24} />
+                    <Image src="/tiktok.png" alt="tiktok" width={24} height={24} />
+
+                </div>
+            </div>
+            <div className={styles.tagList}>
+                <div className={styles.tag}>
+                    <span>Links</span>
+                    <Link href={'#'}>Homepage</Link>
+                    <Link href={'#'}>Blog</Link>
+                    <Link href={'#'}>About</Link>
+                    <Link href={'#'}>Contact</Link>
+                </div>
+                <div className={styles.tag}>
+                    <span>Tags</span>
+                    <Link href={'#'}>Style</Link>
+                    <Link href={'#'}>Fashion</Link>
+                    <Link href={'#'}>Coding</Link>
+                    <Link href={'#'}>Travel</Link>
+                </div>
+                <div className={styles.tag}>
+                    <span>Socials</span>
+                    <Link href={'#'}>Facebook</Link>
+                    <Link href={'#'}>Instagram</Link>
+                    <Link href={'#'}>Tiktok</Link>
+                    <Link href={'#'}>Youtube</Link>
+                </div>
+            </div>
+        </footer>
     );
 }
 
