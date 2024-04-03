c5e934f2e4560c05bc8ca1fe18e9b6b3717af69e
ttchien <chientt.00.student@fit.tdc.edu.vn>
2024-04-04 00:58:14 +0700

    Css for UI of single page

--- src/components/cardList/CardList.jsx
+++ src/components/cardList/CardList.jsx
@@ -48,34 +48,6 @@ const CardList = () => {
                                 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias illum architecto rerum deserunt aperiam impedit esse. Odit similique libero vitae numquam, molestiae id assumenda deserunt ad necessitatibus. Inventore, tenetur incidunt.
                             </p>
                             <Link className={styles.readMore} href={'/'}>Read more</Link>
-
-                        </div>
-                    </div>
-                    <div className={styles.post}>
-                        <div className={styles.imagePost}>
-                            <Image className={styles.image} src={'/culture.png'} alt={'post'} fill />
-                        </div>
-                        <div className={styles.contentPost}>
-                            <p className={styles.typeCategory}>2024-04-03 &#8226; <span>CODING</span></p>
-                            <h3 className={styles.titlePost}>Post 1</h3>
-                            <p className={styles.description}>
-                                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias illum architecto rerum deserunt aperiam impedit esse. Odit similique libero vitae numquam, molestiae id assumenda deserunt ad necessitatibus. Inventore, tenetur incidunt.
-                            </p>
-                            <Link className={styles.readMore} href={'/'}>Read more</Link>
-
-                        </div>
-                    </div>
-                    <div className={styles.post}>
-                        <div className={styles.imagePost}>
-                            <Image className={styles.image} src={'/culture.png'} alt={'post'} fill />
-                        </div>
-                        <div className={styles.contentPost}>
-                            <p className={styles.typeCategory}>2024-04-03 &#8226; <span>CODING</span></p>
-                            <h3 className={styles.titlePost}>Post 1</h3>
-                            <p className={styles.description}>
-                                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias illum architecto rerum deserunt aperiam impedit esse. Odit similique libero vitae numquam, molestiae id assumenda deserunt ad necessitatibus. Inventore, tenetur incidunt.
-                            </p>
-                            <Link className={styles.readMore} href={'/'}>Read more</Link>
                         </div>
                     </div>
                 </div>
